a92851f0ac804f5492da79642b9310ccb98b3127
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2023-07-15 13:45:29 +0500

    Lecture 3
    Primitive types - Understanding Lists & Dictionaries

---
+++ 01-PythonIntro/classpractice.py
@@ -0,0 +1,5 @@
+# This is the line where I am delaring a list
+my_dict = {'key1':122, 'key2':123, 'key3':124, 'key4':[1,2,3], 'key5':['a','b','c']}
+# print(my_dict) not to display this code line.
+# getting value of key1
+print(my_dict['key1'])
